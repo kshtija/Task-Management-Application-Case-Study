a90e063341bef9f36bc3fe8aa74e76371f10d2ce
Kshitija Gaikar <kshitija.gaikar@teknopoint.local>
2025-11-28 18:39:06 +0530

    origin changes for cors

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,15 @@ const authRoutes=require('./routes/authRoutes');
 const taskRoutes=require('./routes/taskRoutes');
 const app=express(); connectDB();
 app.use(express.json());
-app.use(cors({origin:process.env.CLIENT_URL,credentials:true}));
+// app.use(cors({origin:process.env.CLIENT_URL,credentials:true}));
+app.use(cors(
+    {
+        origin: [
+            "http://localhost:3000/",
+            "https://willowy-cassata-b5bdeb.netlify.app/signin"
+        ]
+    }
+))
 app.get('/',(req,res)=>res.send('Task API running'));
 app.use('/api/auth',authRoutes); app.use('/api/tasks',taskRoutes);
 app.listen(process.env.PORT||5000,()=>console.log('Server running'));
